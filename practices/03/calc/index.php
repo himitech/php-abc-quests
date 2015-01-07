cb0bd600aaa30a84bfce5568bf924871e48691fe
himitech <gyagigoo@gmail.com>
2015-01-07 23:17:10 +0900

    POSTをGETに変更、など

--- practices/03/calc/index.php
+++ practices/03/calc/index.php
@@ -1,49 +1,44 @@
 <?php
-$left = isset($_POST['left']) ? $_POST['left'] : '';
-$operator = isset($_POST['operator']) ? $_POST['operator'] : '';
-$right = isset($_POST['right']) ? $_POST['right'] : '';
+$left = isset($_GET['left']) ? $_GET['left'] : '';
+$operator = isset($_GET['operator']) ? $_GET['operator'] : '';
+$right = isset($_GET['right']) ? $_GET['right'] : '';
 
-if ($left && $operator && $right) {
+if (!is_null($left) && !is_null($right)) {
     switch ($operator) {
-        case '−':
-            $answer = $_POST['left'] - $_POST['right'];
-            $selected['−'] = 'selected';
+        case '-':
+            $answer = $left - $right;
             break;
-        case '×':
-            $answer = $_POST['left'] * $_POST['right'];
-            $selected['×'] = 'selected';
+        case '*':
+            $answer = $left * $right;
             break;
-        case '÷':
-            $answer = $_POST['left'] / $_POST['right'];
-            $selected['÷'] = 'selected';
+        case '/':
+            $answer = $left / $right;
             break;
-        case '＋':
+        case '+':
         default:
-            $answer = $_POST['left'] + $_POST['right'];
-            $selected['＋'] = 'selected';
+            $answer = $left + $right;
             break;
     }
-
     $result = "{$left} {$operator} {$right} ＝ {$answer}";
 } else {
     $result = '計算結果なし';
-    $selected['＋'] = 'selected';
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8"/>
     <title>test</title>
 </head>
 <body>
-    <form action="index.php" method="post">
+    <form action="index.php" method="GET">
         <input type="text" name="left" value="<?php echo $left; ?>" required autofocus/>
         <select name="operator">
-            <option value="＋" <?php echo $selected['＋']; ?>>+</option>
-            <option value="−" <?php echo $selected['−']; ?>>-</option>
-            <option value="×" <?php echo $selected['×']; ?>>×</option>
-            <option value="÷" <?php echo $selected['÷']; ?>>÷</option>
+            <option value="+" <?php if ($operator === '+') { echo 'selected'; } ?>>+</option>
+            <option value="-" <?php if ($operator === '-') { echo 'selected'; } ?>>-</option>
+            <option value="*" <?php if ($operator === '*') { echo 'selected'; } ?>>*</option>
+            <option value="/" <?php if ($operator === '/') { echo 'selected'; } ?>>/</option>
         </select>
         <input type="text" name="right" value="<?php echo $right; ?>" required/>
         <input type="submit" value="計算する">
