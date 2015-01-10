1f1f5d39106cb66c5f344f84e968f35e06f4534d
himitech <gyagigoo@gmail.com>
2015-01-10 18:26:12 +0900

    htmlspecialchars対応

--- practices/03/calc/index.php
+++ practices/03/calc/index.php
@@ -33,16 +33,23 @@ if (!is_null($left) && !is_null($right)) {
 </head>
 <body>
     <form action="index.php" method="GET">
-        <input type="text" name="left" value="<?php echo $left; ?>" required autofocus/>
+        <input type="text" name="left" value="<?php echo h($left); ?>" required autofocus/>
         <select name="operator">
             <option value="+" <?php if ($operator === '+') { echo 'selected'; } ?>>+</option>
             <option value="-" <?php if ($operator === '-') { echo 'selected'; } ?>>-</option>
             <option value="*" <?php if ($operator === '*') { echo 'selected'; } ?>>*</option>
             <option value="/" <?php if ($operator === '/') { echo 'selected'; } ?>>/</option>
         </select>
-        <input type="text" name="right" value="<?php echo $right; ?>" required/>
+        <input type="text" name="right" value="<?php echo h($right); ?>" required/>
         <input type="submit" value="計算する">
     </form>
-    <p><?php echo $result; ?></p>
+    <p><?php echo h($result); ?></p>
 </body>
 </html>
+
+<?php
+function h($str)
+{
+    return htmlspecialchars($str, ENT_QUOTES);
+}
+?>
