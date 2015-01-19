099d575bb02a51fefa5c00fc1ffc71ab91b47d7c
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-01-19 22:41:59 +0900

    メール送信機能追（メールアドレスは仮）

--- practices/03/calc/index.php
+++ practices/03/calc/index.php
@@ -1,27 +1,46 @@
 <?php
-$left = isset($_GET['left']) ? $_GET['left'] : '';
-$operator = isset($_GET['operator']) ? $_GET['operator'] : '';
-$right = isset($_GET['right']) ? $_GET['right'] : '';
+define('WEBMASTER_EMAIL', 'hogehoge@gmail.com');
 
-if (!is_null($left) && !is_null($right)) {
-    switch ($operator) {
-        case '-':
-            $answer = $left - $right;
-            break;
-        case '*':
-            $answer = $left * $right;
-            break;
-        case '/':
-            $answer = $left / $right;
-            break;
-        case '+':
-        default:
-            $answer = $left + $right;
-            break;
-    }
-    $result = "{$left} {$operator} {$right} ＝ {$answer}";
-} else {
-    $result = '計算結果なし';
+$left = isset($_GET['left']) ? $_GET['left'] : NULL;
+$operator = isset($_GET['operator']) ? $_GET['operator'] : '+';
+$right = isset($_GET['right']) ? $_GET['right'] : NULL;
+$result = '計算結果なし';
+
+switch (strtolower($_SERVER['REQUEST_METHOD'])) {
+    case 'post':
+        if (isset($_POST['result'])) {
+            $body =
+                "簡易電卓プログラムの記念報告メールです。\n" .
+                "\n" .
+                "計算内容：{$_POST['result']}\n" .
+                "IPアドレス：{$_SERVER['REMOTE_ADDR']}\n"
+                ;
+            mb_language('Japanese');
+            mb_internal_encoding('UTF-8');
+            mb_send_mail(WEBMASTER_EMAIL, '簡易電卓プログラム記念報告', $body, 'From: ' . mb_encode_mimeheader('簡易電卓プログラム') . ' <no-reply@example.com>');
+        }
+
+    case 'get':
+    default:
+        if(!is_null($left) && !is_null($right)){
+            switch ($operator) {
+                case '-':
+                    $answer = $left - $right;
+                    break;
+                case '*':
+                    $answer = $left * $right;
+                    break;
+                case '/':
+                    $answer = $left / $right;
+                    break;
+                case '+':
+                default:
+                    $answer = $left + $right;
+                    break;
+            }
+            $result = "{$left} {$operator} {$right} ＝ {$answer}";
+        }
+        break;
 }
 ?>
 
@@ -41,10 +60,21 @@ if (!is_null($left) && !is_null($right)) {
             <option value="/" <?php if ($operator === '/') { echo 'selected'; } ?>>/</option>
         </select>
         <input type="text" name="right" value="<?php echo h($right); ?>" required/>
-        <input type="submit" value="計算する">
+        <input type="submit" value="計算する"/>
     </form>
     <p><?php echo h($result); ?></p>
-</body>
+
+    <hr>
+
+<?php if (isset($answer) && $answer % 100 === 0) { ?>
+    <form action="index.php" method="POST">
+        <input type="hidden" name="result" value="<?php echo h($result); ?>" />
+        <input type="submit" value="メールで報告"/>
+    </form>
+<?php } ?>
+    <?php echo "<br>answer = "; pre_var_dump($answer); ?>
+    <?php echo "<br>ip = "; pre_var_dump($_SERVER['REMOTE_ADDR']); ?>
+ </body>
 </html>
 
 <?php
@@ -52,4 +82,10 @@ function h($str)
 {
     return htmlspecialchars($str, ENT_QUOTES);
 }
+
+ function pre_var_dump($var){
+    echo "<pre>";
+    var_dump($var);
+    echo "</pre>";
+ }
 ?>
